d4bf687b4ecbb9595c8cb22fa1c06a43eb909685
aj96818 <aj96818@gmail.com>
2021-12-10 18:02:50 -0500

    Added 'Price', 'Quantity' fields to Python script and Mysql database table.

--- crypto_tracker.sql
+++ crypto_tracker.sql
@@ -1,16 +1,19 @@
 create database crypto_tracker_db;
 
-drop database py_tk_crypto_tracker;
+# drop database py_tk_crypto_tracker;
 
 use crypto_tracker_db;
 
-# drop table txns;
+drop table txns;
 
 create table txns (
 	txn_id int not null auto_increment
     , txn_date date
     , symbol VARCHAR(20)
     , txn_type varchar(15)
+    , exchange_name varchar(100)
+    , price decimal(12, 4)
+    , quantity decimal(12, 4)
     , primary key (txn_id)
 ); 
 
--- crypto_tracking_app_v2.py
+++ crypto_tracking_app_v2.py
@@ -4,61 +4,71 @@ import mysql.connector as mysql
 
 class MainWindow:
 
+
     def __init__(self, mw):
 
         self.mw = mw
         mw.geometry("700x400")
-        mw.title("Crypto Ledger App")
+        mw.title("My Crypto Ledger")
         mw.configure(bg="gray12")
 
         self.txn_date_label = tk.Label(mw, text='Date of Transaction:', font=('bold', 15))
         self.txn_date_label.place(x=25, y=30)
-
-        self.symbol_label = tk.Label(mw, text='Crypto Ticker:', font=('bold', 15))
-        self.symbol_label.place(x=25, y=80)
-
-        self.txn_type_label = tk.Label(mw, text='Buy or Sell:', font=('bold', 15))
-        self.txn_type_label.place(x=25, y=120)
-
-        self.exchange_label = tk.Label(mw, text='Exchange:', font=('bold', 15))
-        self.exchange_label.place(x=25, y=160)
-
         self.txn_date_input = tk.Entry()
         self.txn_date_input.place(x=185, y=30)
 
+        self.symbol_label = tk.Label(mw, text='Crypto Ticker:', font=('bold', 15))
+        self.symbol_label.place(x=25, y=80)
         self.symbol_input = tk.Entry()
         self.symbol_input.place(x=185, y=80)
 
+        self.txn_type_label = tk.Label(mw, text='Buy or Sell:', font=('bold', 15))
+        self.txn_type_label.place(x=25, y=120)
         self.txn_type_input = tk.Entry()
         self.txn_type_input.place(x=185, y=120)
 
+        self.exchange_label = tk.Label(mw, text='Exchange:', font=('bold', 15))
+        self.exchange_label.place(x=25, y=160)
         self.exchange_input = tk.Entry()
         self.exchange_input.place(x=185, y=160)
 
-        self.insert = tk.Button(mw, text="insert", font=("italic", 14), bg="blue", command=self.insert)
+        self.price_label = tk.Label(mw, text='Price:', font=('bold', 15))
+        self.price_label.place(x=400, y=120)
+        self.price_input = tk.Entry()
+        self.price_input.place(x=470, y=120)
+
+        self.quantity_label = tk.Label(mw, text='Quantity:', font=('bold', 15))
+        self.quantity_label.place(x=400, y=160)
+        self.quantity_input = tk.Entry()
+        self.quantity_input.place(x=470, y=160)
+
+        self.insert = tk.Button(mw, text="Insert", highlightbackground='#3E4149', font=("bold", 14), bg="blue", command=self.insert)
         self.insert.place(x=20, y=210)
+
+
 
     def insert(self):
 
         date = self.txn_date_input.get()
         symbol = self.symbol_input.get()
         txn_type = self.txn_type_input.get()
-        # exchange = self.exchange_input.get()
+        exchange_name = self.exchange_input.get()
+        price = self.price_input.get()
+        quantity = self.quantity_input.get()
 
         if date == "" or symbol == "" or txn_type == "":
             MessageBox.showinfo("Enter Txn data", "All fields are required.")
         else:
             con = mysql.connect(host="localhost", user="root", password="mysqlrootpw", database="crypto_tracker_db")
             mycursor = con.cursor()
-            sql = "INSERT INTO txns (txn_date, symbol, txn_type) VALUES (%s, %s, %s);"
-            val = (date, symbol, txn_type)
+            sql = "INSERT INTO txns (txn_date, symbol, txn_type, exchange_name, price, quantity) VALUES (%s, %s, %s, %s, %s, %s);"
+            val = (date, symbol, txn_type, exchange_name, price, quantity)
             mycursor.execute(sql, val)
             # cursor.execute("insert into txns values('"+ str(1) + "', '" + date +"','" + symbol + "', '" + txn_type +"')")
             mycursor.execute("commit")
 
             MessageBox.showinfo("Txn inserted into DB successfully", "Successful Insertion");
             con.close()
-
 
 
 root = tk.Tk()
