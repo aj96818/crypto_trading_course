fe441012877ca00f8b66c68b2970a062c6db10f8
aj96818 <aj96818@gmail.com>
2021-12-12 17:07:14 -0500

    Added 'Avg Price' query

--- crypto_tracker.sql
+++ crypto_tracker.sql
@@ -14,10 +14,41 @@ create table txns (
     , exchange_name varchar(100)
     , price decimal(12, 4)
     , quantity decimal(12, 4)
+    , fee decimal(12,4)
     , primary key (txn_id)
 ); 
 
-select * from txns;
+SET GLOBAL local_infile = 1;
+
+LOAD DATA INFILE "/Users/alanjackson/dumps/cb_historical_fills.csv"
+INTO TABLE txns
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
+
+
+
+-- Queries (Begin)
+
+select * from txns order by txn_id desc;
+
+-- Avg Price 
+
+SELECT
+    symbol
+    , ROUND(SUM(quantity * price) / SUM(quantity), 2) AS `Avg Cost`
+    , COUNT(*) AS COUNT
+    , SUM(fee) AS Fees
+FROM
+    txns
+WHERE
+    txn_type = 'BUY'
+GROUP BY
+    symbol
+
 
 
 -- /Users/alanjackson/PycharmProjects/CryptoTradingProject/crypto_trading_bot/crypto_tracking_app.py
