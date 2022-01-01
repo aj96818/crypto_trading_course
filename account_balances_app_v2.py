bc77f6cc7a86196367be271fdcadf31f6ddc2af0
aj96818 <aj96818@gmail.com>
2022-01-01 16:25:50 -0700

    Tested and ready to deploy.  Can start entering actual crypto account balances into mysql 'accounts' table.

--- account_balances_app_v2.py
+++ account_balances_app_v2.py
@@ -1,10 +1,15 @@
+
 import tkinter as tk
+import tkinter.messagebox as MessageBox
+import mysql.connector as mysql
 from tkinter import TclError
 
+
 class SampleApp(tk.Tk):
+
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
-        self.geometry("900x400")
+        self.geometry("700x400")
         self.title("account_balances_app_v2.py")
         self.configure(bg="#49A")
 
@@ -12,11 +17,11 @@ class SampleApp(tk.Tk):
         self.text_box.delete('1.0', 'end')
         self.text_box.place(x=185, y=200)
         self.paste_button = tk.Button(self, text="Paste", command=self.doit)
-        self.paste_button.place(x=480, y=220)
+        self.paste_button.place(x=100, y=240)
         self.doit()
 
         self.clear_button = tk.Button(self, text="Clear", command=self.clear_text_input)
-        self.clear_button.place(x=590, y=220)
+        self.clear_button.place(x=100, y=280)
         self.clear_text_input()
 
         self.txn_date_label = tk.Label(self, text='Last Updated:', font=('bold', 15))
@@ -61,6 +66,48 @@ class SampleApp(tk.Tk):
         self.wallet_label.place(x=25, y=200)
         # self.wallet_input = tk.Entry()
         # self.wallet_input.place(x=185, y=200)
+
+        self.insert = tk.Button(self, text="Insert", highlightbackground='#3E4149', font=("bold", 14), bg="blue", command=self.insert)
+        self.insert.place(x=300, y=340)
+
+        self.clear = tk.Button(self, text="Clear Form", highlightbackground='#3E4149', font=("bold", 14), bg="blue", command=self.clear_entry)
+        self.clear.place(x=390, y=340)
+
+    def insert(self):
+
+        date = self.txn_date_input.get()
+        symbol = self.symbol_input.get().upper()
+        crypto_name = self.crypto_name_input.get()
+        exchange_name = self.clicked.get()
+        price = self.price_input.get()
+        quantity = self.quantity_input.get()
+        notes = self.notes_input.get()
+        wallet = self.text_box.get('1.0', 'end')
+
+        if date == "" or symbol == "" or exchange_name == "":
+            MessageBox.showinfo("Enter Txn data", "All fields are required.")
+        else:
+            con = mysql.connect(host="localhost", user="root", password="mysqlrootpw", database="crypto_tracker_db")
+            mycursor = con.cursor()
+            sql = "INSERT INTO accounts (last_updated, symbol, crypto_name, quantity, location, url, wallet_address, notes) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);"
+            val = (date, symbol, crypto_name, price, exchange_name, quantity, wallet, notes)
+            mycursor.execute(sql, val)
+            # cursor.execute("insert into txns values('"+ str(1) + "', '" + date +"','" + symbol + "', '" + txn_type +"')")
+            mycursor.execute("commit")
+
+            MessageBox.showinfo("Record inserted into DB successfully", "Successful Insertion");
+            con.close()
+
+    def clear_entry(self):
+
+        #        self.txn_date_input.delete("0", "end")
+        self.symbol_input.delete("0", "end")
+        self.crypto_name_input.delete("0", "end")
+        self.clicked.set("")
+        self.price_input.delete("0", "end")
+        self.quantity_input.delete("0", "end")
+        self.notes_input.delete("0", "end")
+        self.text_box.delete("1.0", "end")
 
     def clear_text_input(self):
         self.text_box.delete("1.0", "end")
